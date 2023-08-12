e34f8884e3f6917e545888408fec5c69954086b9
prashdhote92 <prashdhote92@gmail.com>
2023-08-13 00:07:18 +0530

    add length converter app

---
+++ length-converter-app/src/components/LengthConverterApp.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LengthConverter = () => {
+
+    return (
+        <h3>LengthConverter</h3>
+    );
+};
+
+export default LengthConverter;
---
+++ length-converter-app/webpack.config.js
@@ -0,0 +1,46 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const deps = require('./package.json').dependencies;
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    devServer: {
+        port: 3001,
+        open: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|tsx|ts)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'length1',
+            filename: 'remoteConverter.js',
+            exposes: {
+                // expose each component
+                './LengthConverter': './src/components/LengthConverterApp',
+            },
+            shared: {
+                ...deps,
+                react: { singleton: true, eager: true, requiredVersion: deps.react },
+                'react-dom': {
+                    singleton: true,
+                    eager: true,
+                    requiredVersion: deps['react-dom'],
+                },
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+};
